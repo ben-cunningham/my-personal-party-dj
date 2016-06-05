4f4d1013b72a8556838c23d456b48e9fe5a10c22
ben-cunningham <bencunningham17@gmail.com>
2016-06-05 16:28:58 -0700

    Get playlists

--- login/views.py
+++ login/views.py
@@ -4,6 +4,12 @@ from django.http import HttpResponse
 import datetime
 import base64
 import requests
+import json
+
+def get_playlists(headers):
+	playlists = requests.get('https://api.spotify.com/v1/me/playlists', headers=headers)
+	return playlists
+
 
 def login(request):
 	url = 'https://accounts.spotify.com/authorize?client_id=f3ee976a08f14c70bcb93f8bc020e019&redirect_uri=https%3A%2F%2Ff0f02446.ngrok.io%2Fcallback%2F&response_type=code'
@@ -18,16 +24,21 @@ def app(request):
 	}
 
 	auth_header = 'f3ee976a08f14c70bcb93f8bc020e019' +':' +'84103952ba0f48f88080090c9b1962a2'
+	encoded = base64.b64encode(auth_header)
 	headers = {
-		'Authorization': 'Basic ' +base64.b64encode(auth_header)
+		'Authorization': 'Basic ' +encoded
 	}
-	print headers
-	print data
 	r = requests.post('https://accounts.spotify.com/api/token', data=data, headers=headers)
-	print r.text
-	return HttpResponse()
+
+	json_data = json.loads(r.text)
+	print json_data
+	headers = {
+		'Authorization': 'Bearer ' + json_data['access_token']
+	}
+	playlists = get_playlists(headers)
+
+	return HttpResponse(playlists)
 
 def main(request):
-	print request
 	return HttpResponse()
 
