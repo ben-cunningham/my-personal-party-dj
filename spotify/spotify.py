53bd8484588d3b4f4de6cd8f1d6f2275d8b87b54
Austin Hendy <ahendy@hotmail.ca>
2016-06-05 19:13:29 -0700

    test func and name replacement

--- spotify/spotify.py
+++ spotify/spotify.py
@@ -1,13 +1,21 @@
 import requests
-from django.conf.models import Number, Profile
+from twill.models import Number
+from login.models import Profile
 
-def handle_spotify_query(number, query):
-	user = Profile.objects.filter(phone=number)
+def handle_spotify_request(number, query):
+	user = Profile.objects.filter(phone=number)[0]
 	active_playlist = user.playlist_id
-	token = user.generate_header()
+	headers = user.generate_header()
+	query = query.replace(" ", "%20")
+
+
+
+	return_values = requests.get('https://api.spotify.com/v1/search?type=track&q=' + query, headers=headers)
+
+	print return_values
 
 
 
 
-
-
+def test():
+	handle_spotify_request('+16042568089', "kanye west power")
