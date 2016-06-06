168d2f80bd24871e2ba6a074e0821b46ace1039d
Austin Hendy <ahendy@hotmail.ca>
2016-06-05 17:36:14 -0700

    adding activity to models field

--- twill/models.py
+++ twill/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 class Number(models.Model):
 	phone = models.CharField(max_length=12)
+	active = models.BooleanField(default=False)
 
-	def get_free():
+	def get_free_number():
 		raise NotImplementedError
